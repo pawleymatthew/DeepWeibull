0c475234bf25b94c3fa83d3393e9318bc0b237f1
Matthew Pawley <matthewpawley@btinternet.com>
2019-11-23 16:15:15 +0000

    first commit

---
+++ deepweibull.py
@@ -0,0 +1,144 @@
+import numpy as np
+import random
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.layers import Masking
+from keras.optimizers import RMSprop
+from keras import backend as k
+from sklearn.preprocessing import normalize
+from scipy.stats import weibull_min
+from scipy.special import gamma
+
+"""
+Inputs:
+    - outcome_actual = an (N,2) tensor with elements t (time-to-event) and d (0=censored, 1=death).
+    - weibull_param_pred = an (N,2) tensor with elements a and b, the predicted Weibull (alpha, beta) parameters. 
+Output:
+    - the negative log-likelihood, i.e. -logL(weibull_param_pred;outcome_actual)
+Description:
+    Computes the (negative log-) likelihood of the obersved survival data given Weibull parameters (one pair per individual).
+"""
+
+def weibull_loglkhd(outcome_actual, weibull_param_pred, name=None):
+    time = outcome_actual[:, 0] # actual time to event
+    status = outcome_actual[:, 1] # actual status (censored/dead)
+    a = weibull_param_pred[:, 0] # alpha
+    b = weibull_param_pred[:, 1] # beta
+    norm_time = (time + 1e-35) / a
+    return -1 * k.mean(status * (k.log(b) + b * k.log(norm_time)) - k.pow(norm_time, b))
+
+"""
+N = number of observations (in training set).
+Inputs:
+    - weibull_param = an (N,2) tensor with elements a and b, the Weibull parameters before the activation layer
+Output:
+    - (a,b) = the Weibull paramters after the activation layer
+Description:
+    A custom activation function that applies expontial to alpha and softplus to beta in the final layer.
+"""
+
+def weibull_activate(weibull_param):
+    a = k.exp(weibull_param[:, 0]) # exponential of alpha
+    b = k.softplus(weibull_param[:, 1]) # softplus of beta
+    a = k.reshape(a, (k.shape(a)[0], 1))
+    b = k.reshape(b, (k.shape(b)[0], 1))
+    return k.concatenate((a, b), axis=1)
+
+"""
+Inputs:
+    - tensors: a list of four tensors, as in output of make_train_test()
+    - learn_rate: the learning rate of the optimisation procedure
+Outputs:
+    - model: the final compiled model
+    - training_history: a summary of the loss and validation loss at each epoch
+    - test_result: a Pandas dataframe with the outcome variables and corresponding predicted Weibull parameters.
+"""
+
+def train_deep_weibull(tensors,learn_rate, epochs, steps_per_epoch, validation_steps):
+
+    """
+    Define the model:
+        - input layer of appropriate length (i.e. number of features)
+        - hidden layer of length 32
+        - relu activation function
+        - hidden layer of length 32
+        - relu activation function
+        - output layer of length 2
+        - exp and softplus activation functions applied (to zeroth and first elements respectively)
+    """
+
+    model = Sequential()
+    model.add(Dense(3, input_dim=tensors["train_x"].shape[1], activation='relu'))
+    model.add(Dense(3, activation='relu'))
+    model.add(Dense(3, activation='relu'))
+    model.add(Dense(2)) # layer with 2 nodes (alpha and beta)
+    model.add(Activation(weibull_activate)) # apply custom activation function (exp and softplus)
+    """
+
+    Compile the model:
+        - using the (negative) log-likelihood for the Weibull as the loss function
+        - using Root Mean Square Prop optimisation (common) and customisable learning rate
+    """
+    model.compile(loss=weibull_loglkhd, optimizer=RMSprop(lr=learn_rate))
+    
+    """
+    Train the model:
+        - using the training set (x and y values) 
+        - validate on the test set (x and y values)
+        - using the user-specified number of epochs, steps_per_epoch, validation_steps
+    """
+    
+    training_history = model.fit(
+        tensors["train_x"], 
+        tensors["train_y"], 
+        epochs=epochs, 
+        steps_per_epoch=steps_per_epoch, 
+        verbose=0,
+        validation_data=(tensors["test_x"], tensors["test_y"]), 
+        validation_steps=validation_steps)
+    
+    history = pd.DataFrame(training_history.history)
+    history['epoch'] = training_history.epoch
+    
+    """
+    Test the final trained model: 
+        - using the training set (x values only)
+        - test_result: shows the time/status values alongside the model's predicted Weibull parameters
+    """
+    
+    test_predict = model.predict(tensors["test_x"], steps=1) 
+    test_predict = np.resize(test_predict, tensors["test_y"].shape) 
+    test_result = np.concatenate((tensors["test_y"], test_predict), axis=1)
+    
+    return ({
+            "model" : model,
+            "training_history" : history,
+            "test_result" : test_result})
+
+"""
+Inputs:
+    - tensors: a list of tensors, e.g. as in output of make_train_test(), but only "test_x","test_y" actually get used.
+    - model:a trained model, as in "model" output from deep_weibull()
+Outputs:
+    - a Pandas dataframe with four columns 
+        - "time","status" (actual values, from "test_y")
+        - "alpha","beta" (the model's predicted Weibull parameters)s
+"""
+
+def test_deep_weibull(tensors, model):
+    test_predict = model.predict(tensors["test_x"], steps=1) # predict Weibull parameters using covariates
+    test_predict = np.resize(test_predict, tensors["test_y"].shape) 
+    test_result = np.concatenate((tensors["test_y"], test_predict), axis=1)
+    test_result = pd.DataFrame({
+        "time" : test_result[:, 0], 
+        "status" : test_result[:, 1],
+        "alpha" : test_result[:, 2],
+        "beta" : test_result[:, 3]
+    })
+    return test_result
---
+++ synthetic_datasets.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import random
+import math
+import tensorflow as tf
+
+"""
+Inputs:
+    N - number of individuals, a positive integer. 
+    N_c - number of censored individuals, an integer in 0 <= N_c < N.
+ Outputs:
+    A pandas dataframe with N rows and 5 columns called "x1","x2","x3","time","status".
+ Description:
+    Creates a dataset of N individuals, N_c of whom are censored. Each individual has three (iid) covariates. 
+    The outcomes, i.e. time to event and censoring indicator, are simulated from a Weibull distribution.
+    The parameters of the Weibull distribution are independent of the covariates.
+    Can be used to test whether a model is able to learn the absence of a relationship between features and target. 
+"""
+
+def synthdata_noise(N,N_c):
+    x = np.random.normal(loc=0.0, scale=1.0, size=(N,3)) # sim covariates (3 per individual)
+    a = 10e-2 + abs(np.random.normal(loc=50.0, scale=10.0, size=N)) # sim alphas (and ensure >0)
+    b = 10e-2 + abs(np.random.normal(loc=1.0, scale=0.05, size=N)) # sim betas (and ensure >0)
+    time = a * np.random.weibull(b) # sim event times from Weibull(a,b)
+    status = np.ones(N) # initialise status indicator (all dead)
+    censored = random.sample(range(0,N), N_c) # randomly select N_c individuals to be censored
+    status[censored] = 0 # set status indicator for censored patients to 0
+    time[censored] = np.random.uniform(low=0,high=time[censored]) # simulate censoring time Uniform(0,old event time)
+    data = {
+        'x1':[item[0] for item in x], 
+        'x2':[item[1] for item in x],
+        'x3':[item[2] for item in x],
+        'time':time,
+        'status':status
+    }
+    df = pd.DataFrame(data)
+    return df
+
+"""
+Inputs:
+    N - number of individuals, a positive integer. 
+    N_c - number of censored individuals, an integer in 0 <= N_c < N.
+ Outputs:
+    A pandas dataframe with N rows and 5 columns called "x1","x2","x3","time","status".
+ Description:
+    Creates a dataset of N individuals, N_c of whom are censored. Each individual has three (iid) covariates. 
+    The outcomes, i.e. time to event and censoring indicator, are simulated from Weibull distribution.
+    The parameters of the Weibull distribution are given by a function of the covariates.
+    Can be used to test whether a model is able to learn how a model learns a mapping from covariates to outcomes.
+"""
+
+def synthdata_custom(N,N_c):
+    x = np.random.normal(loc=0.0, scale=1.0, size=(N,3)) # sim covariates (3 per individual)
+    a = [max(10e-2,50+(10/math.sqrt(3.0))*(item[0]**2+item[1]-1)) for item in x] # compute alphas (ensure >0)
+    b = [max(10e-2,1+0.05*item[2]) for item in x] # compute betas (ensure >0)
+    time = a * np.random.weibull(b) # sim event times from Weibull(a,b)
+    status = np.ones(N) # initialise status indicator (all dead)
+    censored = random.sample(range(0,N), N_c) # randomly select N_c individuals to be censored
+    status[censored] = 0 # set status indicator for censored to 0
+    time[censored] = np.random.uniform(low=0,high=time[censored]) # simulate censoring time Uniform(0,old event time)
+    data = {
+        'x1':[item[0] for item in x], 
+        'x2':[item[1] for item in x],
+        'x3':[item[2] for item in x],
+        'time':time,
+        'status':status
+    }
+    df = pd.DataFrame(data)
+    return df
+
+"""
+Inputs:
+    - df: a Pandas dataframe (N rows) with feature columns (names don't matter) and outcome columns called 'time' and 'status'
+    - train_frac: the fraction of rows to be allocated to the training set
+Outputs:
+    - a named list of tensors:
+        - 'train_x' containing the training set features
+        - 'train_y' containing the training set outcomes
+        - 'test_x' containing the test set features
+        - 'test_y' containing the test set outcomes
+Description:
+    Creates the training and test sets in the form (tensors) required for use in the model(s).
+"""
+def make_train_test(df, train_frac):
+    # randomly select inds to go in train/test sets
+    train_x = df.sample(frac=train_frac)
+    test_x = df.drop(train_x.index)
+    # remove time/status columns and put them in y train/test sets
+    train_y = pd.DataFrame([train_x.pop(colname) for colname in ['time', 'status']]).T 
+    test_y = pd.DataFrame([test_x.pop(colname) for colname in ['time', 'status']]).T
+    # convert all to tensors
+    train_x = tf.convert_to_tensor(train_x.values, tf.float32)
+    test_x = tf.convert_to_tensor(test_x.values, tf.float32)
+    train_y = tf.convert_to_tensor(train_y.values, tf.float32)
+    test_y = tf.convert_to_tensor(test_y.values, tf.float32)
+    # return named list of tensors
+    return ({"train_x" : train_x,
+             "train_y" : train_y,
+             "test_x" : test_x,
+             "test_y" : test_y})
